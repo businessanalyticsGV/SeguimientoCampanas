a70b0d40bbd75c824c9293d82e254d992797befb
Graciela Rosas Sanchez <gracielarosas@grupovidanta.com>
2019-04-11 19:36:19 -0500

    Cambios menores

--- PromotionCode.py
+++ PromotionCode.py
@@ -4,7 +4,7 @@ import numpy as np
 Base_busqueda = pd.read_csv('E:\Procesos Sense\DB Blast\ListaPC_Busqueda.csv')
 ##print(Base_busqueda)
 
-Catalogo_Codigos=pd.read_excel('E:\Procesos Sense\DB Blast\Catalogo Prueba Blast.xlsx')
+Catalogo_Codigos=pd.read_excel('E:\Procesos Sense\DB Blast\Catalogo Blast.xlsx')
 
 Base_busqueda['Marca'] = [str([code for code in Catalogo_Codigos['PromotionCode'] if code in prom]) for prom in Base_busqueda['PromotionCode']]
 Base_busqueda['Marca'] = [prom.replace('[',"") for prom in Base_busqueda['Marca']]
@@ -15,6 +15,6 @@ Base_busqueda.rename(columns = {'PromotionCode':'PromotionCodeOrig','Marca':'Pro
 Base_busqueda = Base_busqueda.merge(Catalogo_Codigos[['PromotionCode','Codigo','fecha de envio blast','Reusado']], how = 'left', on = ['PromotionCode'])
 
 #Base_busqueda['Codigo']=np.where(Base_busqueda['DateCreated']>=Base_busqueda['fecha de envio blast'])
-Base_busqueda.to_excel('E:\Procesos Sense\DB Blast\Base_Prueba.xlsx')
+Base_busqueda.to_excel('E:\Procesos Sense\DB Blast\Base_PromotionCode_Trabajo.xlsx')
 
 Base_busqueda.to_csv('Base_Prueba.csv')
